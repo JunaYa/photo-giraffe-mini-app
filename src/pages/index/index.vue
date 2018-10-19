d5f1411da55368b198b0efd9148ecdb7be8d57f9
Yajun Wang <ayac3j@gmail.com>
2018-10-19 15:56:02 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      userInfo: {},
+    };
+  },
+
+  components: {
+  },
+
+  methods: {
+    getUserInfo() {
+      wx.login({
+        success: () => {
+          wx.getUserInfo({
+            success: (res) => {
+              this.userInfo = res.userInfo;
+            },
+          });
+        },
+      });
+    },
+  },
+
+  created() {
+    this.getUserInfo();
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
