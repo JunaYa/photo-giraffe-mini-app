4610145fad4f94453d4e266247e8e3945aa06d28
Yajun Wang <ayac3j@gmail.com>
2018-10-19 17:31:17 +0800

    complete image load

---
+++ src/components/Picture.vue
@@ -0,0 +1,27 @@
+<template>
+  <image
+    :src="picture.path"
+    class="picture"
+    mode="aspectFit"
+  >
+  </image>
+</template>
+
+<script>
+    export default {
+      name: 'Picture',
+      props: {
+        picture: {
+          type: Object,
+          required: true,
+        },
+      },
+    };
+</script>
+
+<style scoped>
+  image {
+    width: 8rem;
+    height: 12rem;
+  }
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,41 +1,72 @@
 <template>
-  <div>
-
+  <div class="container">
+    <div class="btn_select_picture"
+         @click="selectPicture">
+      选取图片
+    </div>
+    <Picture
+      v-if="picture"
+      :picture="picture"/>
   </div>
 </template>
 
 <script>
+  import Picture from '@/components/Picture';
 
-export default {
-  data() {
-    return {
-      userInfo: {},
-    };
-  },
-
-  components: {
-  },
-
-  methods: {
-    getUserInfo() {
-      wx.login({
-        success: () => {
-          wx.getUserInfo({
-            success: (res) => {
-              this.userInfo = res.userInfo;
-            },
-          });
-        },
-      });
+  export default {
+    data() {
+      return {
+        picture: null,
+      };
     },
-  },
 
-  created() {
-    this.getUserInfo();
-  },
-};
+    components: {
+      Picture,
+    },
+
+    methods: {
+      getUserInfo() {
+        wx.login({
+          success: () => {
+            wx.getUserInfo({
+              success: (res) => {
+                this.userInfo = res.userInfo;
+              },
+            });
+          },
+        });
+      },
+
+      selectPicture() {
+        wx.chooseImage({
+          count: 1,
+          sourceType: ['album', 'camera'],
+          sizeType: ['original', 'compressed'],
+          success: (res) => {
+            if (res.tempFiles.length > 0) {
+              this.getPictureInfo(res.tempFiles[0].path).then((info) => {
+                this.picture = info;
+              });
+            }
+          },
+        });
+      },
+
+      getPictureInfo(url) {
+        return new Promise((resolve) => {
+          wx.getImageInfo({
+            src: url,
+            success: data => resolve(data),
+          });
+        });
+      },
+
+    },
+  };
 </script>
 
 <style scoped>
+  .container {
 
+  }
 </style>
